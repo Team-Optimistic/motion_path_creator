5ec2691bd78a362f1bc37b561a2a24de9253b5a3
Octogonapus <firey45@gmail.com>
2016-10-13 23:39:43 -0400

    Implemented odom callback

--- include/motion_path_creator/mp_creator.h
+++ include/motion_path_creator/mp_creator.h
@@ -3,20 +3,35 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <ros/ros.h>
 #include <string>
+#include <geometry_msgs/Point32.h>
+#include <nav_msgs/Odometry.h>
 
 class mpCreator
 {
 public:
   mpCreator()
   {
-    sub = n.subscribe<sensor_msgs::PointCloud2>("cloud", 10, &mpCreator::callback, this);
+    objSub = n.subscribe<geometry_msgs::Point32>("cloud", 10, &mpCreator::objCallback, this);
+    odomSub = n.subscribe<nav_msgs::Odometry>("odometry/filtered", 1000, &mpCreator::odomCallback, this);
   }
 
-  void callback(const sensor_msgs::PointCloud2::ConstPtr& in)
+  void objCallback(const geometry_msgs::Point32::ConstPtr& in)
   {
+  }
 
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& in)
+  {
+    x = in->pose.pose.position.x;
+    y = in->pose.pose.position.y;
+    const geometry_msgs::Quaternion quat = in->pose.pose.orientation;
+    theta = atan2((2 * ((quat.x * quat.w) + (quat.y * quat.z))),
+                  ((quat.x * quat.x) + (quat.y * quat.y) - (quat.z * quat.z) - (quat.w * quat.w)));
   }
 
   ros::NodeHandle n;
-  ros::Subscriber sub;
+  ros::Subscriber objSub, odomSub;
+
+  geometry_msgs::Point32 nextObject;
+private:
+  float x, y, theta;
 };
