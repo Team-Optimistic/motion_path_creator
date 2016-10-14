5a61f76443c4adc3073793fdf0770b9d2c266543
Octogonapus <firey45@gmail.com>
2016-10-14 00:00:05 -0400

    Added vel to odom callback

--- include/motion_path_creator/mp_creator.h
+++ include/motion_path_creator/mp_creator.h
@@ -17,6 +17,10 @@ public:
 
   void objCallback(const geometry_msgs::Point32::ConstPtr& in)
   {
+    //The next object we pick up should be the one which is both:
+    // close to us, and
+    // in our direction of movement (no sense in turning around to get the
+    // "technically" closest object)
   }
 
   void odomCallback(const nav_msgs::Odometry::ConstPtr& in)
@@ -26,6 +30,9 @@ public:
     const geometry_msgs::Quaternion quat = in->pose.pose.orientation;
     theta = atan2((2 * ((quat.x * quat.w) + (quat.y * quat.z))),
                   ((quat.x * quat.x) + (quat.y * quat.y) - (quat.z * quat.z) - (quat.w * quat.w)));
+
+    xVel = in->twist.twist.linear.x;
+    yVel = in->twist.twist.linear.y;
   }
 
   ros::NodeHandle n;
@@ -34,4 +41,5 @@ public:
   geometry_msgs::Point32 nextObject;
 private:
   float x, y, theta;
+  float xVel, yVel;
 };
