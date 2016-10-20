3615573b41ae582a2e299d07c527f43db409e891
Octogonapus <firey45@gmail.com>
2016-10-20 00:15:59 -0400

    Bug fixes

--- include/motion_path_creator/mp_creator.h
+++ include/motion_path_creator/mp_creator.h
@@ -26,7 +26,7 @@ public:
   /**
    * Callback for robotPOS request for closest object behind robot
    */
-  void mpCreator::robotPOSCallback(const std_msgs::Empty::ConstPtr& in); //const geometry_msgs::Point32::ConstPtr& in
+  void robotPOSCallback(const std_msgs::Empty::ConstPtr& in); //const geometry_msgs::Point32::ConstPtr& in
 private:
   ros::NodeHandle n;
   ros::Publisher mpcPub;
