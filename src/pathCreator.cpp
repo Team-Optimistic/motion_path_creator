791f3c57885b1e9d46d77ea69bb6847782871c1e
Octogonapus <firey45@gmail.com>
2017-03-20 23:16:12 -0400

    Fixed formatting

--- src/pathCreator.cpp
+++ src/pathCreator.cpp
@@ -45,8 +45,10 @@ int main(int argc, char **argv)
   {
     rate.sleep();//ensure even the new msg check maxes at 100 hz
     ros::spinOnce(); //Callbacks
+    
     if(!mpc.isNewMessage())
       continue;
+
     std::vector<geometry_msgs::Point32> objList, finalObjList;
     geometry_msgs::Point32 coords = mpc.getCoords();
 
@@ -57,7 +59,6 @@ int main(int argc, char **argv)
     pose.pose.position.y = coords.y;
     pose.pose.position.z = 0;
     pose.pose.orientation = tf::createQuaternionMsgFromYaw(coords.z);
-
 
     //Add small objects
     for (auto&& obj : mpc.getSmallObjs().points)
@@ -80,7 +81,7 @@ int main(int argc, char **argv)
         return sortByCost(coords, a, getTypeCost(a), b, getTypeCost(b));
       });
 
-      float oldZ = objList.front().z;
+      float oldZ = objList.front().z; //save obj type
 
       //Add cheapest element to final list and remove it from overall list
       finalObjList.push_back(objList.front());
@@ -106,6 +107,7 @@ int main(int argc, char **argv)
     path.poses.reserve(finalObjList.size() + 1);
     path.poses.push_back(pose); // starting position
 
+    //Fill path points
     for (int i = 0; i < finalObjList.size(); i++)
     {
       pose.header.stamp = ros::Time::now();
@@ -115,132 +117,134 @@ int main(int argc, char **argv)
       pose.pose.orientation = tf::createQuaternionMsgFromYaw(finalObjList.at(i).z);
       path.poses.push_back(pose);
     }
+
     //Publish path
     pathPub.publish(path);
-
   }
   return 0;
 }
 
-
-
 /**
- * Publishes a number of objects
- * @param numObjs Number of objects to publish
- * @param objs    Object vector to read from
- * @param pub     Publisher to publish with
- */
- void publishObjects(const int numObjs, const std::vector<geometry_msgs::Point32> objs, const ros::Publisher pub)
- {
+* Publishes a number of objects
+* @param numObjs Number of objects to publish
+* @param objs    Object vector to read from
+* @param pub     Publisher to publish with
+*/
+void publishObjects(const int numObjs, const std::vector<geometry_msgs::Point32> objs, const ros::Publisher pub)
+{
   //Convert objList to PointCloud
   sensor_msgs::PointCloud temp;
   temp.header.stamp = ros::Time::now();
   temp.header.frame_id = "/field";
   temp.points = objs;
   pub.publish(temp);
- // ROS_INFO("mpc: sent %d",temp.points.size());
+  // ROS_INFO("mpc: sent %d",temp.points.size());
 }
 
 /**
- * Computes the distance from the a point to a point
- * @param  from The point to measure from (point 1)
- * @param  to   The point to measure to (point 2)
- * @return      The distance between the points
- */
- inline const float distanceToPoint(const geometry_msgs::Point32& from, const geometry_msgs::Point32& to)
- {
+* Computes the distance from the a point to a point
+* @param  from The point to measure from (point 1)
+* @param  to   The point to measure to (point 2)
+* @return      The distance between the points
+*/
+inline const float distanceToPoint(const geometry_msgs::Point32& from, const geometry_msgs::Point32& to)
+{
   return sqrt(pow(to.x - from.x, 2) + pow(to.y - from.y, 2));
 }
 
 /**
- * Computes the angle between two points
- * @param  from The point to measure from (point 1)
- * @param  to   The point to measure to (point 2)
- * @return      The angle between the points
- */
- inline const float angleToPoint(const geometry_msgs::Point32& from, const geometry_msgs::Point32& to)
- {
+* Computes the angle between two points
+* @param  from The point to measure from (point 1)
+* @param  to   The point to measure to (point 2)
+* @return      The angle between the points
+*/
+inline const float angleToPoint(const geometry_msgs::Point32& from, const geometry_msgs::Point32& to)
+{
   return atan2(to.y - from.y, to.x - from.x);
 }
 
 /**
- * Returns the cost for the type of an object
- * @param  obj Object to use
- * @return     Type cost of object
- */
- inline const float getTypeCost(const geometry_msgs::Point32& obj)
- {
+* Returns the cost for the type of an object
+* @param  obj Object to use
+* @return     Type cost of object
+*/
+inline const float getTypeCost(const geometry_msgs::Point32& obj)
+{
   constexpr float smallObjectCost = 1/1.5, bigObjectCost = 1/4;
 
   switch (int(obj.z))
   {
     case ObjTypes::small:
-    return smallObjectCost;
+      return smallObjectCost;
 
     case ObjTypes::big:
-    return bigObjectCost;
+      return bigObjectCost;
 
     default:
-    return std::numeric_limits<int>::max();
+      return std::numeric_limits<int>::max();
   }
 }
 
 /**
- * Computes the cost to pick up an object
- * @param  robot      Where the robot is
- * @param  object     Where the object is
- * @param  objectCost The cost of the object type
- * @return            The cost of the object
- */
- inline const float getCost(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& object, const float objectCost)
- {
+* Computes the cost to pick up an object
+* @param  robot      Where the robot is
+* @param  object     Where the object is
+* @param  objectCost The cost of the object type
+* @return            The cost of the object
+*/
+inline const float getCost(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& object, const float objectCost)
+{
   constexpr float moveCost = 1, turnCost = 2;
-  float turnDistance =(angleToPoint(robot, object) - robot.z) * (180.0 / M_PI);
+
+  float turnDistance = (angleToPoint(robot, object) - robot.z) * (180.0 / M_PI);
+
   turnDistance = turnDistance > 180 ? turnDistance - 360 : turnDistance;
   turnDistance = turnDistance < -180 ? turnDistance + 360 : turnDistance;
   turnDistance = turnDistance < 0 ? turnDistance * -1 : turnDistance;
-  float straightDistance = distanceToPoint(robot, object);
+
+  const float straightDistance = distanceToPoint(robot, object);
+
   return (straightDistance * moveCost + turnDistance * turnCost) * objectCost;
 }
 
 /**
- * Computes which object has lesser cost
- * @param  robot    Where the robot is
- * @param  obj1     First object
- * @param  obj1Cost Cost of first object's type
- * @param  obj2     Second object
- * @param  obj2Cost Cost of second object's type
- * @return          Whether the first object has lower cost than the second
- */
- bool sortByCost(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& obj1, const float obj1Cost, const geometry_msgs::Point32& obj2, const float obj2Cost)
- {
+* Computes which object has lesser cost
+* @param  robot    Where the robot is
+* @param  obj1     First object
+* @param  obj1Cost Cost of first object's type
+* @param  obj2     Second object
+* @param  obj2Cost Cost of second object's type
+* @return          Whether the first object has lower cost than the second
+*/
+bool sortByCost(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& obj1, const float obj1Cost, const geometry_msgs::Point32& obj2, const float obj2Cost)
+{
   return getCost(robot, obj1, obj1Cost) <= getCost(robot, obj2, obj2Cost);
 }
 
 /**
- * Computes which object has lesser distance
- * @param  robot    Where the robot is
- * @param  obj1     First object
- * @param  obj1Cost Cost of first object's type
- * @param  obj2     Second object
- * @param  obj2Cost Cost of second object's type
- * @return          Whether the first object is closer than the second
- */
- bool sortByDistance(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& obj1, const float obj1Cost, const geometry_msgs::Point32& obj2, const float obj2Cost)
- {
+* Computes which object has lesser distance
+* @param  robot    Where the robot is
+* @param  obj1     First object
+* @param  obj1Cost Cost of first object's type
+* @param  obj2     Second object
+* @param  obj2Cost Cost of second object's type
+* @return          Whether the first object is closer than the second
+*/
+bool sortByDistance(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& obj1, const float obj1Cost, const geometry_msgs::Point32& obj2, const float obj2Cost)
+{
   return distanceToPoint(robot, obj1) <= distanceToPoint(robot, obj2);
 }
 
 /**
- * Computes which object has lesser angle
- * @param  robot    Where the robot is
- * @param  obj1     First object
- * @param  obj1Cost Cost of first object's type
- * @param  obj2     Second object
- * @param  obj2Cost Cost of second object's type
- * @return          Whether the first object is closer than the second
- */
- bool sortByAngle(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& obj1, const float obj1Cost, const geometry_msgs::Point32& obj2, const float obj2Cost)
- {
+* Computes which object has lesser angle
+* @param  robot    Where the robot is
+* @param  obj1     First object
+* @param  obj1Cost Cost of first object's type
+* @param  obj2     Second object
+* @param  obj2Cost Cost of second object's type
+* @return          Whether the first object is closer than the second
+*/
+bool sortByAngle(const geometry_msgs::Point32& robot, const geometry_msgs::Point32& obj1, const float obj1Cost, const geometry_msgs::Point32& obj2, const float obj2Cost)
+{
   return angleToPoint(robot, obj1) <= angleToPoint(robot, obj2);
 }
